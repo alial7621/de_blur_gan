97cd5a5597026ddbb9e551e9b15045cc0b188a03
alial7621 <a_lashini@yahoo.com>
2025-04-20 11:22:28 +0330

    updating train.py

--- train.py
+++ train.py
@@ -170,7 +170,7 @@ def train(args):
                 critic_loss += (final_critic_loss.item() / args.critic_update)
 
             # Compute metrics
-            train_SSIM += SSIM(sharp_images, generated_images, device)
+            train_SSIM += (SSIM(sharp_images, generated_images, device)*100)
             train_PSNR += PSNR(sharp_images, generated_images, device)
 
             ###########################
@@ -213,7 +213,7 @@ def train(args):
         train_PSNR_history.append(train_PSNR)
 
         print("Step/Epoch: %03d || Generator Train Loss: %.2f  Critic Loss: %.2f || SSIM Train: %.4f  PSNR Train: %.4f"
-              %(cur_epoch, gen_loss, critic_loss, (train_SSIM*100), train_PSNR))
+              %(cur_epoch, gen_loss, critic_loss, train_SSIM, train_PSNR))
 
         # Evaluate the model on Test/Val data
         generator.eval()
@@ -224,7 +224,7 @@ def train(args):
             with torch.no_grad():
                 generated_images = generator(blury_images)
             
-            test_SSIM += SSIM(sharp_images, generated_images, device)
+            test_SSIM += (SSIM(sharp_images, generated_images, device)*100)
             test_PSNR += PSNR(sharp_images, generated_images, device)
 
             gen_test_loss += percept_loss(sharp_images, generated_images, val=True)
@@ -240,7 +240,7 @@ def train(args):
         gen_test_loss_history.append(gen_test_loss)
 
         print("Generator Test Loss: %.2f|| SSIM Test: %.4f  PSNR Test: %.4f\n"
-              %(gen_test_loss, (test_SSIM*100), test_PSNR))
+              %(gen_test_loss, test_SSIM, test_PSNR))
 
         gen_scheduler.step(test_SSIM)
         critic_scheduler.step(critic_loss)
